15e0cd5b4d13ab89e4ae369f362c66eaafc2784e
tighug <kapuriko123@gmail.com>
2020-11-04 09:13:28 +0900

    chore: Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -4,9 +4,9 @@ module.exports = {
   port: process.env.DB_PORT || 3306,
   username: process.env.DB_USERNAME || "test",
   password: process.env.DB_PASSWORD || "test",
-  database: process.env.DB_DATABASE || "test",
-  synchronize: true,
-  logging: false,
+  database: process.env.DB_NAME || "test",
+  synchronize: true, // FIXME: Danger
+  logging: process.env.NODE_ENV === "development",
   entities: [
     process.env.NODE_ENV === "production"
       ? "dist/domain/model/**/*.js"

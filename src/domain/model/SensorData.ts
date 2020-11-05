48091c5429724d9affcbedbdcee66bb5a503abfd
tighug <kapuriko123@gmail.com>
2020-11-06 07:20:14 +0900

    feat: Update SensorData.ts

--- src/domain/model/SensorData.ts
+++ src/domain/model/SensorData.ts
@@ -2,30 +2,28 @@ import {
   Column,
   CreateDateColumn,
   Entity,
-  ManyToOne,
   PrimaryGeneratedColumn,
 } from "typeorm";
-import { Sensor } from "./Sensor";
 
 @Entity()
 export class SensorData {
   @PrimaryGeneratedColumn()
   readonly id?: number;
 
-  @ManyToOne(() => Sensor)
-  readonly sensor!: Sensor;
+  @Column()
+  readonly deviceName!: string;
 
   @Column()
-  readonly temperature?: number;
+  readonly temperature!: number;
 
   @Column()
-  readonly humidity?: number;
+  readonly humidity!: number;
 
   @Column()
-  readonly pressure?: number;
+  readonly pressure!: number;
 
   @Column()
-  readonly gas?: number;
+  readonly gas!: number;
 
   @CreateDateColumn()
   readonly createdAt?: Date;
@@ -34,7 +32,7 @@ export class SensorData {
     // This is a hack for TypeORM.
     if (props) {
       this.id = props.id;
-      this.sensor = props.sensor;
+      this.deviceName = props.deviceName;
       this.temperature = props.temperature;
       this.humidity = props.humidity;
       this.pressure = props.pressure;
